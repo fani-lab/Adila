8911e588d166c5e800fd988d4f10d0fd629ab00d
hosseinfani <hosseinfani@gmail.com>
2023-03-08 18:07:19 -0500

    code refactor

--- src/common/metric_plot.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt
-import numpy as np
-def metric_plot(plt_ndkl_before,plt_ndkl_after,baseline,metric_name):
-    # change line 5 and 6 with different values, for value x since they need to be coded dynamically later with a lambda function
-    plt.scatter(np.arange(len(plt_ndkl_before)),plt_ndkl_before,label=f'{metric_name} before')
-    plt.scatter(np.arange(len(plt_ndkl_before)),plt_ndkl_after,label=f'{metric_name} after')
-    plt.legend()
-    plt.savefig(f'{baseline}metric-{metric_name}.png')
-    plt.show()
