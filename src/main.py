expu calculation for after reranking bug fix

src/main.py
@@ -12,7 +12,7 @@ from fairsearchcore.models import FairScoreDoc
 import reranking
 
 from cmn.metric import *
-from util.fair_greedy import fairness_greedy
+#from util.fair_greedy import fairness_greedy
 from util.visualization import area_under_curve
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -217,9 +217,15 @@ class Reranking:
                     try: dic_before[metric]['nonprotected'].append(per_group_exp_before[True])
                     except KeyError: dic_before['exposure']['nonprotected'].append(0)
                     dic_before[metric][metric] = exp_before
-                    exp_after, per_group_exp_after = frt.Metrics.EXP(pd.DataFrame(data=reranked_idx[i][:k_max]), dict([(j, labels[j]) for j in reranked_idx[i][:k_max]]), 'MinMaxRatio')
-                    dic_after[metric]['protected'].append(per_group_exp_after[False]), dic_after[metric]['nonprotected'].append(per_group_exp_after[True])
-                    dic_after[metric][metric] = exp_after
+
+                    if metric == 'exp':
+                        exp_after, per_group_exp_after = frt.Metrics.EXP(pd.DataFrame(data=reranked_idx[i][:k_max]), dict([(j, labels[j]) for j in reranked_idx[i][:k_max]]), 'MinMaxRatio')
+                        dic_after[metric]['protected'].append(per_group_exp_after[False]), dic_after[metric]['nonprotected'].append(per_group_exp_after[True])
+                        dic_after[metric][metric] = exp_after
+                    elif metric == 'expu':
+                        exp_after, per_group_exp_after = frt.Metrics.EXPU(pd.DataFrame(data=reranked_idx[i][:k_max]), dict([(j, labels[j]) for j in reranked_idx[i][:k_max]]), pd.DataFrame(data=[j[2] for i in reranked_idx[i][:k_max] for j in member_probs if j[0] == i]), 'MinMaxRatio')
+                        dic_after[metric]['protected'].append(per_group_exp_after[False]), dic_after[metric]['nonprotected'].append(per_group_exp_after[True])
+                        dic_after[metric][metric] = exp_after
 
             df_before = pd.DataFrame(dic_before[metric]).mean(axis=0).to_frame('mean.before')
             df_after = pd.DataFrame(dic_after[metric]).mean(axis=0).to_frame('mean.after')
@@ -462,7 +468,6 @@ if __name__ == "__main__":
             print(f'Parallel run started ...')
             with multiprocessing.Pool(multiprocessing.cpu_count() if params.settings['core'] < 0 else params.settings['core']) as executor:
                 executor.starmap(partial(Reranking.run,
-
                                          fsplits=args.fsplits,
                                          fairness_notion=args.fairness_notion,
                                          att=args.att,